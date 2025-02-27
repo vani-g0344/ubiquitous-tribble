02b566fe88daa897dd787d2f00927487f546f852
VANI G <vanig0344@gmail.com>
2025-02-27 22:03:45 +0530

    Add Changes

--- oops.cpp
+++ oops.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+class Person{
+    public:
+        string name;
+        int age;
+    Person(string name, int age){
+        this->name = name;
+        this->age = age;
+    }
+};
+
+class Student : public Person{
+    public:
+        int rollNo;
+    Student(string name, int age, int rollNo) : Person(name, age){
+        this->rollNo = rollNo;
+    }
+};
 
 
 int main(){
